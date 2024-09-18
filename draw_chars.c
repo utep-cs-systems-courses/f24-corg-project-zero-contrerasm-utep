89c30d0cb747eaaaa1dbe1b6076bb61d249f89c6
macontreras11 <macontreras11@miners.utep.edu>
2024-09-18 12:04:19 -0600

    finished

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,17 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+void print_char_11x16(char c)
+
+{
+  c -= 0x20;
+  for (char col = 0; col <16 ; col++) {
+    for (char row = 0; row < 11; row++) {
+      unsigned short rowBits = font_11x16[c][row];
+      unsigned short colMask = 1 << col; /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
